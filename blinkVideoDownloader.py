57e94a506c0d47bb1585b8e120878c4be9037525
Ernest Mallet <ernest.mallett@localmed.com>
2018-12-16 12:25:43 -0600

    Issue #20 and Issue #19 rest uri fix

--- blinkVideoDownloader.py
+++ blinkVideoDownloader.py
@@ -16,6 +16,10 @@ headers = {
 data = '{ "password" : "' + sys.argv[2] + '", "client_specifier" : "iPhone 9.2 | 2.2 | 222", "email" : "' + sys.argv[1] + '" }'
 res = requests.post('https://rest.prod.immedia-semi.com/login', headers=headers, data=data)
 authToken = res.json()["authtoken"]["authtoken"]
+region = res.json()["region"]
+region = list(region.keys())[0]
+#print(region)
+
 print(authToken)
 
 headers = {
@@ -25,14 +29,14 @@ headers = {
 
 pageNum = 0
 while True:
-    pageNumUrl = 'https://prod.immedia-semi.com/api/v2/videos/page/' + str(pageNum)
+    pageNumUrl = 'https://rest-'+region+'.immedia-semi.com/api/v2/videos/page/' + str(pageNum)
     print(pageNumUrl)
     res = requests.get(pageNumUrl, headers=headers)
     videoListJson = res.json()
     if not videoListJson:
         break
     for videoJson in videoListJson:
-        mp4Url = 'https://prod.immedia-semi.com' + videoJson["address"]
+        mp4Url = 'https://rest-'+region+'.immedia-semi.com' + videoJson["address"]
         print(mp4Url)
         res = requests.get(mp4Url, headers=headers, stream=True)
         with open(os.path.basename(mp4Url)[14:], 'wb') as out_file:
