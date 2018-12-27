01b75b4df820c9e47f505055672322538e54afa0
JohnMurga <john@murga.org>
2018-12-27 21:27:03 +0800

    Timezone from video, better filenames, skip already downloaded, ensure download succesful

--- blinkVideoDownloader.py
+++ blinkVideoDownloader.py
@@ -4,6 +4,12 @@ import requests
 import shutil
 import os
 import sys
+import json
+
+from datetime import datetime
+import pytz
+
+import os.path
 
 if len(sys.argv) != 3:
     print('usage: ' + sys.argv[0] + ' email password')
@@ -18,8 +24,8 @@ res = requests.post('https://rest.prod.immedia-semi.com/login', headers=headers,
 authToken = res.json()["authtoken"]["authtoken"]
 region = res.json()["region"]
 region = list(region.keys())[0]
-#print(region)
 
+print(region)
 print(authToken)
 
 headers = {
@@ -27,18 +33,31 @@ headers = {
     'TOKEN_AUTH': authToken,
 }
 
-pageNum = 0
+fileFormat = "%Y-%m-%d %H-%M-%S"
+pageNum = 1  # Page actually appear to start at one
 while True:
     pageNumUrl = 'https://rest-'+region+'.immedia-semi.com/api/v2/videos/page/' + str(pageNum)
-    print(pageNumUrl)
+    print("## Processing page - " + str(pageNum) + " ##")
     res = requests.get(pageNumUrl, headers=headers)
     videoListJson = res.json()
     if not videoListJson:
         break
     for videoJson in videoListJson:
+        # print(json.dumps(videoJson, indent=4, sort_keys=True))
         mp4Url = 'https://rest-'+region+'.immedia-semi.com' + videoJson["address"]
-        print(mp4Url)
-        res = requests.get(mp4Url, headers=headers, stream=True)
-        with open(os.path.basename(mp4Url)[10:], 'wb') as out_file:
-            shutil.copyfileobj(res.raw, out_file)
+        datetime_object = datetime.strptime(videoJson["created_at"], '%Y-%m-%dT%H:%M:%S+00:00')
+        utcmoment = datetime_object.replace(tzinfo=pytz.utc)
+        localDatetime = utcmoment.astimezone(pytz.timezone(videoJson["time_zone"]))
+        fileName = localDatetime.strftime(fileFormat) + " - " + videoJson["camera_name"] + " - " + videoJson["network_name"] + ".mp4"
+
+        if os.path.isfile(fileName):
+            print(" * Skipping " + fileName + " *")
+            # print("Already downloaded (diff : " + str((int(videoJson["size"])-os.path.getsize(fileName))) + " bytes)")
+        else:
+            # print("Downloading - " + mp4Url)
+            print("Saving - " + fileName)
+            res = requests.get(mp4Url, headers=headers, stream=True)
+            with open("tmp-download", 'wb') as out_file:
+                shutil.copyfileobj(res.raw, out_file)
+            os.rename("tmp-download", fileName)
     pageNum += 1
